86dbb924ad234e0006f1e586f14ffeb4fe1e5f4e
Conor Le <lethanhvu2004@gmail.com>
2025-02-26 01:51:03 -0500

    Initial Commit

---
+++ COMP3097-G10-ToDoApp/COMP3097_G10_ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  COMP3097_G10_ToDoAppApp.swift
+//  COMP3097-G10-ToDoApp
+//
+//  Created by Thành Vũ Lê on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct COMP3097_G10_ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COMP3097-G10-ToDoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  COMP3097-G10-ToDoApp
+//
+//  Created by Thành Vũ Lê on 2025-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

b6990f9c840aeac90525e77e2582262b941969e4
Conor Le <lethanhvu2004@gmail.com>
2025-02-27 01:09:59 -0500

    Add task view added and navigation to it from main view

---
+++ COMP3097-G10-ToDoApp/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  COMP3097-G10-ToDoApp
+//
+//  Created by Thành Vũ Lê on 2025-02-27.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
